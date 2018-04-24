d1a92271b968b2b7c0fdc6c7b562ecc6f6f92328
yanOfWHU <873534617@qq.com>
2018-04-24 23:03:16 +0800

    commit

--- src/main/java/com/spring/bbs/service/impl/AccountServiceImpl.java
+++ src/main/java/com/spring/bbs/service/impl/AccountServiceImpl.java
@@ -1,5 +1,8 @@
 package com.spring.bbs.service.impl;
 
+import com.spring.bbs.dao.AccountDao;
+import com.spring.bbs.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,5 +12,8 @@ import org.springframework.stereotype.Service;
  * @Version 1.0
  */
 @Service
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    AccountDao accountDao;
 }
