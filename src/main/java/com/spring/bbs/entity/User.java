fc1b93191f2a51d2f19ddf88be98c6939351fea2
yanOfWHU <873534617@qq.com>
2018-04-24 13:01:59 +0800

    modify user test

--- src/main/java/com/spring/bbs/entity/User.java
+++ src/main/java/com/spring/bbs/entity/User.java
@@ -7,5 +7,36 @@ package com.spring.bbs.entity;
  * @Version 1.0
  */
 public class User {
+    private Integer id;
+    private String userName;
+    private String password;
 
+    public User(){
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
