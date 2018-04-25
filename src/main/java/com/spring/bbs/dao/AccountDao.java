eeedf3668208f49ff0b6d61e1db96858ac0a5432
RickyZhou44 <819894286@qq.com>
2018-04-25 19:02:16 +0800

    commit all

--- src/main/java/com/spring/bbs/dao/AccountDao.java
+++ src/main/java/com/spring/bbs/dao/AccountDao.java
@@ -1,14 +1,20 @@
 package com.spring.bbs.dao;
 
+import com.spring.bbs.entity.Account;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * @Description TODO
- * @Author 严旭江 Yan 873534617@qq.com
+ * @Author RickZhou
  * @date 2018/4/24-22:09
  * @Version 1.0
  */
 @Repository
 public interface AccountDao {
-    AccountDao findAccount(String accountName,String password);
+    public Account queryByName(@Param("accountName") String accountName);
+    public List<Account> queryAll();
+    public void insertAccount(@Param("accountName")String accountName,@Param("password") String password,@Param("email") String email);
 }
